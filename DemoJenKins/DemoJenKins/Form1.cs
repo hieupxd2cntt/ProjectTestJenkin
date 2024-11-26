7899d81915832bc1af9b46f905ee5a5421663aae
hieupx <hieupham05@gmail.com>
2024-11-26 07:34:57 +0700

    Test#1

--- DemoJenKins/DemoJenKins/Form1.cs
+++ DemoJenKins/DemoJenKins/Form1.cs
@@ -6,5 +6,10 @@ namespace DemoJenKins
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
     }
 }
